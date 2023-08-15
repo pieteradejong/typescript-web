f971f0fefd0b2c0af56cb345a9c89b5544417dfd
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-14 23:24:12 -0400

    test eslint github action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello there.');
+import { User } from "./models/User";
+
+const user = new User({ name: 'myname', age: 21 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
