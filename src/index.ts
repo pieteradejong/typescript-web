332aa39e8885bc3efc0c50ac2015d7be17fc5294
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-15 21:19:53 -0400

    add event listeners to user model. add json server with db stub

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,14 @@ import { User } from "./models/User";
 
 const user = new User({ name: 'myname', age: 21 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+    console.log('fmvkdlsjvfs');
+});
+user.on('change', () => {
+    console.log('907867');
+});
+user.on('save', () => {
+    console.log('SAVE fd');
+});
+
+user.trigger('save');
